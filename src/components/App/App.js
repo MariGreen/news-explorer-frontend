ea72c84523faf58f1c66597773a01b2da883beab
Marina Grishina <marisupova@yandex.ru>
2020-12-21 04:04:04 +0300

    begin

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="page">
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Footer.css';
+import gitHub from '../../images/footer_github.png';
+import faceBook from '../../images/footer_fb.png';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer__copyright">&copy; 2020  Supersite, Powered by News API</p>
+      <nav className="footer__nav">
+      <ul className="footer__items">
+        <li className="footer__item"><p className="footer__text"> Главная</p></li>
+        <li className="footer__item"><a href="https://yandex.praktikum/" target="_blank" rel="noreferrer" className="footer__link"><p className="footer__text"> Яндекс.Практикум</p></a></li>
+        <li className="footer__item"><a href="#" target="_blank" rel="noreferrer" className="footer__link"><img className="footer__icon" alt='Github icon' src={gitHub}/></a>
+        <a href="#" target="_blank" rel="noreferrer" className="footer__link"><img className="footer__icon"alt='Github icon' src={faceBook}/></a></li>             
+      </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Footer;
