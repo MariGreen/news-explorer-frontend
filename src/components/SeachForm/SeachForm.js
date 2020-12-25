799908e64ebe631e93ccf78d752970ac897ab4ec
Marina Grishina <marisupova@yandex.ru>
2020-12-26 01:26:21 +0300

    adaptiv 1

--- src/components/SeachForm/SeachForm.js
+++ src/components/SeachForm/SeachForm.js
@@ -4,13 +4,11 @@ import './SeachForm.css';
 function SeachForm(params) {
   return (
     <div className="seach-form">
-      <div className="seach-form__text">
+      <div className="seach-form__item">
         <h1 className="seach-form__title">Что творится в мире?</h1>
         <p className="seach-form__subtitle">Находите самые свежие статьи на&nbsp;любую тему и&nbsp;сохраняйте в&nbsp;своём личном кабинете.</p>
-        <div className="seach-form__item">
-          <input className="seach-form__input-field" type="text" placeholder="Введите тему новости" />
-          <button className="seach-form__submit" type="button" aria-label="найти новости">Искать</button>
-        </div>
+        <input className="seach-form__input-field" type="text" placeholder="Введите тему новости" />
+        <button className="seach-form__submit" type="button" aria-label="найти новости">Искать</button>
       </div>
 
     </div>
