45af421f0e169504abdbe4fcc677b3120e10effc
Marina Grishina <marisupova@yandex.ru>
2020-12-31 01:43:30 +0300

    card

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,23 @@ import React from 'react';
 /* import Card from './Card';
 import Loader from './Loader'; */
 import './Main.css';
+import '../NewsCard/NewsCard.css';
 
 function Main(props) {
   /* const currentUser = React.useContext(CurrentUserContext);
   const initialLoading = React.useContext(InitialLoadingContext);   */
+
+  const { news } = props;
+
+  function handleTrashClick() {
+    props.onTrashClick(props.news);
+  }
+
+  function handleAdviceClick() {
+    props.onTrashClick(props.news);
+  }
+
+  const isSaved = true;
 
   return (
     <div className="content">
@@ -14,6 +27,20 @@ function Main(props) {
           <h2 className="result__title"> Результаты поиска</h2>
           <div className="result__show-results">
             <div className="result__cards-imitation"></div>
+            {/* // */}
+            <div className="news-card">
+              <img alt={news.name} className="news-card__picture" src={news.link} />
+              <div className="news-card__data">
+                <div className="news-card__header">
+                  {isSaved && <button type="submit" className="news-card__key-word" >Природа</button>}
+                  {isSaved && <button type="submit" className="news-card__hint" onClick={handleAdviceClick}>Убрать из сохранеённых</button>}
+                  {isSaved && <button type="submit" className="news-card__trash" onClick={handleTrashClick}></button>}
+                </div>
+                <p className="news-card__date">{news.publishAt}</p>
+
+              </div>
+            </div>
+            {/* // */}
             <div className="result__cards-imitation"></div>
             <div className="result__cards-imitation"></div>
             <div className="result__cards-imitation"></div>
@@ -25,7 +52,7 @@ function Main(props) {
             aria-label="показать больше результатов"
             className="result__show-button"
           // onClick={props.onAddPlace}
-          ></button>
+          >Показать еще</button>
         </div>
       </section>
 
